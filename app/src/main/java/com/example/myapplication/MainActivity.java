be01295d8eb56ee9c2edffd43f3268577f8b3022
AlfredLouisCarpio <alfredlouiscarpio29@gmail.com>
2023-02-11 17:32:20 +0800

    Initial MyApplication project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "This is an info log");
         Log.w(TAG, "This is a warn log");
         Log.e(TAG, "This is a error log");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+            }
+        });
 
     }
 }
